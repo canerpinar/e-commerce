89aa75207989c20bcffe9f0f417f8a9be9a577ba
ITU-Lab <canerpnr@gmail.com>
2020-07-23 16:18:12 +0300

    post method

--- web/src/main/java/org/web/config/SecurityConfig.java
+++ web/src/main/java/org/web/config/SecurityConfig.java
@@ -9,6 +9,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests().antMatchers("/").permitAll()
-                .anyRequest().authenticated();
+                .and().authorizeRequests().antMatchers("/signin").permitAll()
+                .anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll();
     }
 }
--- web/src/main/java/org/web/controller/IndexController.java
+++ web/src/main/java/org/web/controller/IndexController.java
@@ -1,7 +1,10 @@
 package org.web.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.web.entity.User;
+
+import java.util.Map;
 
 @Controller
 public class IndexController {
@@ -13,5 +16,17 @@ public class IndexController {
     @RequestMapping("/profile")
     public String profile(){
         return "profile";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping(value = "/signin",method = RequestMethod.POST)
+    @ResponseBody
+    public void signin(@RequestParam Map<String,String> params){
+        System.out.println(params.get("username"));
+        //return "signin";
     }
 }
